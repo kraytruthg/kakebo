57ef3078a7167fd108135fee40572af7ee2ab0ba
Jerry Chen <jerry@tripla.jp>
2026-02-28 09:30:23 +0800

    fix: restrict accounts routes to implemented actions only

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   get "budget", to: "budget#index", as: :budget
-  resources :accounts do
+  resources :accounts, only: [:index, :show, :new, :create, :edit, :update] do
     resources :transactions, only: [:create, :destroy]
   end
   get "reports", to: "reports#index", as: :reports
