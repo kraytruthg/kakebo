9fa0a7757558fbf6f8c047773516cdcb5daa7a85
Jerry Chen <jerry@tripla.jp>
2026-03-02 00:00:15 +0800

    feat: add transfers routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :accounts, only: [:index, :show, :new, :create, :edit, :update] do
     resources :transactions, only: [:create, :destroy, :edit, :update]
   end
+  resources :transfers, only: [:new, :create, :destroy]
   get "budget/:year/:month/categories/:category_id/transactions",
       to: "budget/category_transactions#index",
       as: :budget_category_transactions
