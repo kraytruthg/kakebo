45b6b6158da135206a3ca31476e8f33a845aa520
Jerry Chen <jerry@tripla.jp>
2026-03-01 12:22:15 +0800

    fix: update user spec for optional household association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   subject { build(:user) }
 
-  it { should belong_to(:household) }
+  it { should belong_to(:household).optional }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email).case_insensitive }
