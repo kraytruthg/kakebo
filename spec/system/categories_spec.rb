d244ffa629fec17c108940b96a4db01ba2bb2858
Jerry Chen <jerry@tripla.jp>
2026-03-03 20:55:56 +0800

    test: add system tests for drag-and-drop ordering and group change

--- spec/system/categories_spec.rb
+++ spec/system/categories_spec.rb
@@ -46,4 +46,44 @@ RSpec.describe "類別管理", type: :system do
     expect(page).to have_text("有交易")
     expect(page).to have_text("筆交易")
   end
+
+  it "拖曳調整 CategoryGroup 順序" do
+    group2 = create(:category_group, household: user.household, name: "娛樂", position: 2)
+    visit settings_categories_path
+
+    source = find("[data-sortable-id='#{group2.id}'] .drag-handle")
+    target = find("[data-sortable-id='#{group.id}'] .drag-handle")
+    source.drag_to(target)
+
+    sleep 1
+    expect(group2.reload.position).to be < group.reload.position
+  end
+
+  it "拖曳調整 Category 順序" do
+    cat1 = create(:category, category_group: group, name: "食物", position: 0)
+    cat2 = create(:category, category_group: group, name: "交通", position: 1)
+    visit settings_categories_path
+
+    source = find(:css, ".divide-y > [data-sortable-id='#{cat2.id}'] .drag-handle")
+    target = find(:css, ".divide-y > [data-sortable-id='#{cat1.id}'] .drag-handle")
+    source.drag_to(target)
+
+    sleep 1
+    expect(cat2.reload.position).to be < cat1.reload.position
+  end
+
+  it "編輯時將 Category 換到其他群組" do
+    group2 = create(:category_group, household: user.household, name: "娛樂")
+    cat = create(:category, category_group: group, name: "電影")
+    visit settings_categories_path
+
+    within(".divide-y[data-controller='sortable'] [data-sortable-id='#{cat.id}']") do
+      find("a[href*='edit']").click
+    end
+    select "娛樂", from: "所屬群組"
+    click_button "儲存"
+
+    expect(page).to have_text("類別已更新")
+    expect(cat.reload.category_group).to eq(group2)
+  end
 end
