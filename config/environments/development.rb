0e2eb0d42e61c96956e34caca33c0b090c06db17
Jerry Chen <jerry@tripla.jp>
2026-03-05 01:20:21 +0800

    chore: allow ngrok tunneling in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,9 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # Allow ngrok tunneling
+  config.hosts << /.*\.ngrok-free\.app/
+
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
