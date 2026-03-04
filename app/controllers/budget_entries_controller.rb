366fb56d45edc0aebbbbd62afe76d783fdd4c668
Jerry Chen <jerry@tripla.jp>
2026-03-04 08:39:16 +0800

    feat: add budget entries show endpoint for inline edit cancel

--- app/controllers/budget_entries_controller.rb
+++ app/controllers/budget_entries_controller.rb
@@ -10,6 +10,17 @@ class BudgetEntriesController < ApplicationController
     )
   end
 
+  def show
+    @category = Category.joins(:category_group)
+                        .where(category_groups: { household_id: Current.household.id })
+                        .find(params[:category_id])
+    @year  = params[:year].to_i
+    @month = params[:month].to_i
+    @entry = BudgetEntry.find_or_initialize_by(
+      category_id: @category.id, year: @year, month: @month
+    )
+  end
+
   def create
     @category = Category.joins(:category_group)
                         .where(category_groups: { household_id: Current.household.id })
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post "budget/copy_from_previous", to: "budget#copy_from_previous", as: :budget_copy_from_previous
   resources :budget_entries, only: [ :create ]
   get "budget_entries/edit", to: "budget_entries#edit", as: :edit_budget_entries
+  get "budget_entries/show", to: "budget_entries#show", as: :show_budget_entry
   resources :accounts, only: [ :index, :show, :new, :create, :edit, :update ] do
     resources :transactions, only: [ :create, :destroy, :edit, :update ]
   end
