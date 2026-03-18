f2a1d269ae0ed734bf84938131e4658c9a2d274c
Jerry Chen <jerry@tripla.jp>
2026-03-18 23:36:20 +0800

    fix: restore execute_script for mobile household delete spec to trigger Stimulus input event

--- spec/system/household_delete_spec.rb
+++ spec/system/household_delete_spec.rb
@@ -75,7 +75,9 @@ RSpec.describe "Household deletion", type: :system do
 
         expect(page).to have_text(second_household.name)
 
-        fill_in "household_name", with: second_household.name
+        field = find_field("household_name")
+        field.fill_in with: second_household.name
+        execute_script("arguments[0].dispatchEvent(new Event('input', { bubbles: true }))", field)
         expect(page).to have_button("永久刪除此帳本", disabled: false)
         click_button "永久刪除此帳本"
 
