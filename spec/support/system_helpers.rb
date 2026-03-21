c9b47897deacec70dc6f26bcde838844e6111a63
Jerry Chen <jerry@tripla.jp>
2026-03-21 19:35:49 +0800

    style: fix rubocop array bracket spacing

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -32,7 +32,7 @@ module SystemHelpers
   def fill_in_with_keyboard(locator, with:)
     field = find_field(locator)
     field.click
-    field.send_keys([:control, "a"], :delete)
+    field.send_keys([ :control, "a" ], :delete)
     field.send_keys(with)
   end
 
